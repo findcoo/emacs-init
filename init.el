1e991647ac96a0242f38c6732ccefedfb66dca9c
coo.find <coo.find@kakaocorp.com>
2019-10-01 22:56:57 +0900

    Fix change projectile mode

--- init.el
+++ init.el
@@ -78,7 +78,7 @@ There are two things you can do about this warning:
 (global-set-key (kbd "C-h v") #'helpful-variable)
 (global-set-key (kbd "C-h k") #'helpful-key)
 
-(projectile-mode +1)
+(counsel-projectile-mode +1)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
